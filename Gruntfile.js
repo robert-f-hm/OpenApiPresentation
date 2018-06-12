ba475fda9259c549d80c30aba14de5cb59f4bf95
François Robert <francois.robert@harmonie-mutuelle.fr>
2018-06-12 22:50:28 +0200

    Ajout style propre à la presentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,7 +132,7 @@ module.exports = function(grunt) {
 				tasks: 'css-themes'
 			},
 			css: {
-				files: [ 'css/reveal.scss' ],
+				files: [ 'css/reveal.scss', 'css/openapi.css' ],
 				tasks: 'css-core'
 			},
 			html: {
